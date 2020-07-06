55c16f7629d613fb54a21c6275f59f071e6222fe
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-07-06 01:05:42 -0300

    User authentication - jwt

---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, 'a00915916befab34b3903204cc5bc27e', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
