606825b4fa399c4d0a40fef80b8e8b52ed6e48c3
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-07-14 00:25:14 -0300

    Authenticated routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" component={SignIn} exact />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
