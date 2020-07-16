c5b252fb0d799de5c2228caea091c8632f500e5d
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-07-16 01:14:34 -0300

    Authentication screen

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu cadastro</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('Foi');
+              }}
+            >
+              Cadastrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn
+        onPress={() => {
+          console.log('Foi');
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#FFF" />
+        <BackToSignInText>Criar uma conta</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
