b303aaf1d200da3daf1af729af9b3f4ca358edeb
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-07-24 22:58:42 -0300

    Starting with TDD, retrieve email feature

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,30 @@
+// import AppError from '@shared/errors/AppError';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to retrieve password using email', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUserRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUserRepository.create({
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
