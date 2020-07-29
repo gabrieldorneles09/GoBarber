4a3683e9f2fb28c825ace5deec9beac4b1d29403
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-07-28 23:54:13 -0300

    Finishing tests

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -1,6 +1,5 @@
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-import User from '@modules/users/infra/typeorm/entities/User';
 import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 import { uuid } from 'uuidv4';
@@ -15,9 +14,19 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      foundToken => foundToken.token === token,
+    );
 
     return userToken;
   }
