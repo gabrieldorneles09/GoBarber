09c258941d4a06e7623141f83833bbad12250216
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-08-05 00:06:36 -0300

    Providers availability routes and controllers

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { day, month, year } = req.body;
+    const { provider_id } = req.params;
+
+    const listProviderDayAvailabilityService = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return res.json(availability);
+  }
+}
