0019e50618ec5acccb3e3fbfe592c3291a3433b4
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2020-08-25 14:39:09 -0300

    Flat list for providers

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
 import {
@@ -10,14 +11,30 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
   const navigateToProfile = useCallback(() => {
-    navigate('Profile');
+    // navigate('Profile');
+    signOut();
   }, [navigate]);
 
   return (
@@ -29,9 +46,15 @@ const Dashboard: React.FC = () => {
         </HeaderTitle>
 
         <ProfileButton onPress={navigateToProfile}>
-          <UserAvatar source={{ uri: user.avatar_utl }} />
+          <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
